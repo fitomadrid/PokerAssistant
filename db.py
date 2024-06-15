6cc477ecc6c193901e913df134eb0dd1329e2eae
fitomadrid <russel379363@gmail.com>
2024-06-15 01:53:59 -0400

    optimization enhancement

--- db.py
+++ db.py
@@ -23,41 +23,44 @@ def db_connect():
 
 def create_hands_table(connection):
     try:
-        cursor = connection.cursor()
-        cursor.execute(SQL_CREATE_HANDS_TABLE)
-        print("Hands table created successfully.")
+        with connection:
+            cursor = connection.cursor()
+            cursor.execute(SQL_CREATE_HANDS_TABLE)
+            print("Hands table created successfully.")
     except sqlite3.Error as error:
         print(f"Error creating hands table: {error}")
 
-def insert_hand_record(connection, poker_hand, outcome):
+def insert_hand_records(connection, poker_hands):
     try:
         insert_query = '''INSERT INTO hands(hand, outcome) VALUES(?,?)'''
-        cursor = connection.cursor()
-        cursor.execute(insert_query, (poker_hand, outcome))
-        connection.commit()
-        print("Poker hand data inserted successfully.")
+        with connection:
+            cursor = connection.cursor()
+            cursor.executemany(insert_query, poker_hands)
+            print(f"{cursor.rowcount} Poker hand data inserted successfully.")
     except sqlite3.Error as error:
         print(f"Error inserting poker hand data: {error}")
 
 def retrieve_hand_records(connection):
     try:
-        select_query = '''SELECT * FROM hands'''
-        cursor = connection.cursor()
-        cursor.execute(select_query)
-        
-        rows = cursor.fetchall()
-        
-        for row in rows:
-            print(row)
+        with connection:
+            select_query = '''SELECT * FROM hands'''
+            cursor = connection.cursor()
+            cursor.execute(select_query)
+            
+            rows = cursor.fetchall()
+            
+            for row in rows:
+                print(row)
     except sqlite3.Error as error:
         print(f"Error fetching poker hands data: {error}")
 
 if __name__ == "__main__":
-    connection = db_connect()
-    
-    if connection:  # Ensure connection was successful before proceeding
-        create_hands_table(connection)
-        insert_hand_record(connection, "5H 5C 6S 7S KD", "Pair of Fives")
-        retrieve_hand_records(connection)
-
-        connection.close()
+    with db_connect() as connection:
+        if connection:
+            create_hands_table(connection)
+            hands_data = [
+                ("5H 5C 6S 7S KD", "Pair of Fives"),
+                ("2C 3S 4S 8C AH", "High Card"),
+            ]
+            insert_hand_records(connection, hands_data)
+            retrieve_hand_records(connection)
